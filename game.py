6719ad17ce7036f25e3d1e1431505cb1c7f70997
Hummenix <diehummel88@gmail.com>
2019-07-13 03:45:17 +0200

    YEETED twice

--- game.py
+++ game.py
@@ -9,8 +9,8 @@ display_height = 600
 gameDisplay = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption('MyGame')
 
-black = (0,0,0)
-white = (255,255,255)
+black = (0, 0, 0)
+white = (255, 255, 255)
 
 clock = pygame.time.Clock()
 crashed = False
