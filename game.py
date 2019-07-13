e756e2ba5c805853ad036469426e7b235c5f0e11
Hummenix <diehummel88@gmail.com>
2019-07-13 05:10:21 +0200

    YEETED trice?

--- game.py
+++ game.py
@@ -11,11 +11,22 @@ pygame.display.set_caption('MyGame')
 
 black = (0, 0, 0)
 white = (255, 255, 255)
+pink  = (255, 90, 90)
+light_blue = (180, 180, 255)
 
 clock = pygame.time.Clock()
 crashed = False
 
-playerSprite = pygame.image.load('./sprites/characters/player.')
+playerSprite = pygame.image.load('./sprites/characters/player.png')
+
+
+def car(x, y):
+    gameDisplay.blit(playerSprite, (x, y))
+
+
+x = (display_width * 0.01)
+y = (display_height * 0.01)
+
 
 while not crashed:
     for event in pygame.event.get():
@@ -24,6 +35,8 @@ while not crashed:
 
         print(event)
 
+    gameDisplay.fill(white)
+    car(x, y)
     pygame.display.update()
     clock.tick(60)
 
