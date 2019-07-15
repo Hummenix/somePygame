6af69699509a9150684924fcf80ba935ec0c1a88
Hummenix <diehummel88@gmail.com>
2019-07-15 21:09:09 +0200

    added media files

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import pygame
+import time
 # from player import player
 
 pygame.init()
@@ -25,6 +26,28 @@ def player(x, y):
     gameDisplay.blit(player_sprite, (x, y))
 
 
+def text_objects(text, font):
+    textSurface = font.render(text, True, pink)
+    return textSurface, textSurface.get_rect()
+
+
+def message_display(text):
+    largeText = pygame.font.Font('freesansbold.ttf', 40)
+    TextSurf, TextRect = text_objects(text, largeText)
+    TextRect.center = ((display_width/2),(display_height/2))
+    gameDisplay.blit(TextSurf, TextRect)
+
+    pygame.display.update()
+
+    time.sleep(3)
+
+    gameLoop()
+
+
+def exitedGameArea():
+    message_display('You exited the game area. Restarting.')
+
+
 def gameLoop():
     x = (display_width * 0.45)
     y = (display_height * 0.8)
@@ -35,24 +58,31 @@ def gameLoop():
     while not gameExit:
         for event in pygame.event.get():
             print(event)
+            # If game is intentionally quit
             if event.type == pygame.QUIT:
-                gameExit = True
+                pygame.quit()
+                quit()
 
+            # if button is pressed
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
                     x_change = -5
                 elif event.key == pygame.K_RIGHT:
                     x_change = 5
+            # if button is released
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                     x_change = 0
 
+        # change horizontal position
         x += x_change
 
         gameDisplay.fill(white)
         player(x, y)
+        # if player is at border
         if x > display_width - player_width or x < 0:
-            gameExit = True
+            exitedGameArea()
+            # gameExit = True
 
         pygame.display.update()
         clock.tick(60)
