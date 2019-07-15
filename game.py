24f14b614f2014f9123ae834f925e1afb19216bb
Hummenix <diehummel88@gmail.com>
2019-07-15 21:28:29 +0200

    whitespace

--- game.py
+++ game.py
@@ -9,7 +9,7 @@ black       = (0, 0, 0)
 white       = (255, 255, 255)
 pink        = (255, 50, 90)
 # light_blue  = (180, 180, 255)
-light_blue  = (173,216,230)
+light_blue  = (173, 216, 230)
 
 # Display/Window dimensions and settings
 display_width = 800
