6dc3b94aa83bcba447a318fd46d4cfe28a91a2b2
Hummenix <diehummel88@gmail.com>
2019-07-16 19:52:33 +0200

    initial

--- game.py
+++ game.py
@@ -24,115 +24,128 @@ player_width    = 64
 player_height   = 64
 
 
-def enemy(enemy_x, enemy_y, enemy_width, enemy_height, enemy_color):
-    pygame.draw.rect(gameDisplay, enemy_color, [enemy_x, enemy_y, enemy_width, enemy_height])
+class enemy():
+
+    def enemy(self, enemy_x, enemy_y, enemy_width, enemy_height, enemy_color):
+        internal_enemy_x = enemy_x
+        internal_enemy_y = enemy_y
+        internal_enemy_width = enemy_width
+        internal_enemy_height = enemy_height
+        internal_enemy_color = enemy_color
+
+        pygame.draw.rect(gameDisplay, internal_enemy_color, [internal_enemy_x, internal_enemy_y, internal_enemy_width, internal_enemy_height])
 
 
-def player(x, y):
-    gameDisplay.blit(player_sprite, (x, y))
+class main(enemy):
 
+    @staticmethod
+    def player(x, y):
+        gameDisplay.blit(player_sprite, (x, y))
 
-def message_display(text, height):
-    largeText = pygame.font.Font('freesansbold.ttf', 40)
-    TextSurf, TextRect = text_objects(text, largeText)
-    TextRect.center = ((display_width/2), (height))
-    gameDisplay.blit(TextSurf, TextRect)
-
-    pygame.display.update()
-
-
-def exitedGameArea():
-    message_display('You exited the game area. Restarting.', text_height_center)
-
-    time.sleep(3)
-    gameLoop()
-
-
-def collidedWithEnemy():
-    message_display('You collided with an enemy.', text_height_center)
-
-    time.sleep(3)
-    gameLoop()
-
-
-def returnHighScore(score):
-    message_display("Your Highscore is " + str(score), text_height_top)
-
-
-def scoreCounter(score):
-    font = pygame.font.SysFont(None, 20)
-    text = font.render("Score: " + str(score), True, project_colors.black)
-    gameDisplay.blit(text, (0, 0))
-
-
-def gameLoop():
-    x = (display_width * 0.45)
-    y = (display_height * 0.8)
-    x_change = 0
-    enemy_start_x = random.randrange(0, display_width)  # -enemy_width)
-    enemy_start_y = -500
-    enemy_speed = 4
-    enemy_width = 100
-    enemy_height = 100
-    score = 0
-    scoreCounter(score)
-    gameExit = False
-
-    while not gameExit:
-        for event in pygame.event.get():
-            print(event)
-            # If game is intentionally quit
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-
-            # if button is pressed
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    x_change = -5
-                elif event.key == pygame.K_RIGHT:
-                    x_change = 5
-            # if button is released
-            if event.type == pygame.KEYUP:
-                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                    x_change = 0
-
-        # change horizontal position
-        x += x_change
-        gameDisplay.fill(project_colors.light_blue)
-        # enemy(enemy_x, enemy_y, enemy_width, enemy_height, enemy_color)
-        enemy_start_y += enemy_speed
-        enemy(enemy_start_x, enemy_start_y, enemy_width, enemy_height, project_colors.light_gray)
-        player(x, y)
-        scoreCounter(score)
-
-        # if player is at border
-        if x > display_width - player_width or x < 0:
-            exitedGameArea()
-
-        if enemy_start_y > display_height:
-            enemy_start_y = 0 - enemy_height
-            enemy_start_x = random.randrange(0, display_width-int(enemy_width))
-            score += 1
-            print("Score: " + str(score))
-            enemy_speed += 0.2
-            enemy_width += (score * 0.1)
-
-        if y < enemy_start_y + enemy_height and y + player_height > enemy_start_y:
-            # print("y-coordinate crossover")
-            # below if statement can be simplified bot this way it's more descriptive
-            if enemy_start_x < x < enemy_start_x + enemy_width \
-                    or enemy_start_x < x + player_width < enemy_start_x + enemy_width:
-                print("Enemy hit \nPlayer position: " + str(x) + " to " + str(x + player_width) + " at height " +
-                      str(y) + "\nEnemy position: " + str(enemy_start_x) + " to " + str(enemy_start_x + enemy_width) +
-                      " at height " + str(enemy_start_y))
-                returnHighScore(score)
-                collidedWithEnemy()
+    @staticmethod
+    def message_display(text, height):
+        largeText = pygame.font.Font('freesansbold.ttf', 40)
+        TextSurf, TextRect = text_objects(text, largeText)
+        TextRect.center = ((display_width / 2), height)
+        gameDisplay.blit(TextSurf, TextRect)
 
         pygame.display.update()
-        clock.tick(60)
+
+    def gameResetAction(self, text):
+        self.message_display(text, text_height_center)
+
+        time.sleep(3)
+        self.gameLoop()
+
+    def returnHighScore(self, score):
+        self.message_display("Your Highscore is " + str(score), text_height_top)
+
+    @staticmethod
+    def scoreCounter(score):
+        font = pygame.font.SysFont(None, 30)
+        text = font.render("Score: " + str(score), True, project_colors.black)
+        gameDisplay.blit(text, (0, 0))
+
+    def gameLoop(self):
+        x = (display_width * 0.45)
+        y = (display_height * 0.8)
+        x_change = 0
+
+        enemy_start_x = random.randrange(0, display_width)  # -enemy_width)
+        enemy_start_y = -500
+        enemy_speed = 4
+        enemy_width = 100
+        enemy_height = 100
+        enemy_list = []
+        enemy_number = 3
+        enemy_color = project_colors.light_gray
+        for each in range(0, enemy_number):
+            # enemy(enemy_x, enemy_y, enemy_width, enemy_height, enemy_color):
+            enemy_list.append(self.enemy(enemy_start_x, enemy_start_y, enemy_width, enemy_height, enemy_color))
+
+        score = 0
+
+        self.scoreCounter(score)
+        gameExit = False
+
+        while not gameExit:
+            for event in pygame.event.get():
+                print(event)
+                # If game is intentionally quit
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+
+                # if button is pressed
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:
+                        x_change = -5
+                    elif event.key == pygame.K_RIGHT:
+                        x_change = 5
+                # if button is released
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                        x_change = 0
+
+            # change horizontal position
+            x += x_change
+            gameDisplay.fill(project_colors.light_blue)
+            # enemy(enemy_x, enemy_y, enemy_width, enemy_height, enemy_color)
+
+            for enemy in enemy_list:
+                enemy.internal_enemy_y += enemy_speed
+                self.enemy(enemy_start_x, enemy_start_y, enemy_width, enemy_height, project_colors.light_gray)
+
+            self.player(x, y)
+            self.scoreCounter(score)
+
+            # if player is at border
+            if x > display_width - player_width or x < 0:
+                self.gameResetAction("You exited the game area. Restarting.")
+
+            if enemy_start_y > display_height:
+                enemy_start_y = 0 - enemy_height
+                enemy_start_x = random.randrange(0, display_width-int(enemy_width))
+                score += 1
+                print("Score: " + str(score))
+                enemy_speed += 0.2
+                enemy_width += (score * 0.1)
+
+            if y < enemy_start_y + enemy_height and y + player_height > enemy_start_y:
+                # print("y-coordinate crossover")
+                # below if statement can be simplified bot this way it's more descriptive
+                if enemy_start_x < x < enemy_start_x + enemy_width \
+                        or enemy_start_x < x + player_width < enemy_start_x + enemy_width:
+                    print("Enemy hit \nPlayer position: " + str(x) + " to " + str(x + player_width) + " at height " +
+                          str(y) + "\nEnemy position: " + str(enemy_start_x) + " to " + str(enemy_start_x + enemy_width)
+                          + " at height " + str(enemy_start_y))
+                    self.returnHighScore(score)
+                    self.gameResetAction("You collided with an enemy.")
+
+            pygame.display.update()
+            clock.tick(60)
 
 
-gameLoop()
+main().gameLoop()
 pygame.quit()
 quit()
