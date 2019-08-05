3644d72fd9d8c5da97f4a2b77f723a009bbae4c9
Hummenix <diehummel88@gmail.com>
2019-08-05 19:39:40 +0200

    removed useless stuff

--- game.py
+++ game.py
@@ -3,37 +3,37 @@ import time
 from text import text_objects
 import project_colors
 import random
+
 # from player import player
 
 pygame.init()
 
 # Display/Window dimensions and settings
 # TODO: export as many functions and variables as possible
-display_width   = 800
-display_height  = 800
-gameDisplay     = pygame.display.set_mode((display_width, display_height))
+display_width = 800
+display_height = 800
+gameDisplay = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption('MyGame')
 clock = pygame.time.Clock()
 
-text_height_top     = display_height/6
-text_height_center  = display_height/2
+text_height_top = display_height / 6
+text_height_center = display_height / 2
 
 # Player width and looks
-player_sprite   = pygame.image.load('./sprites/characters/player64.png')
-player_width    = 64
-player_height   = 64
+player_sprite = pygame.image.load('./sprites/characters/player64.png')
+player_width = 64
+player_height = 64
 
 
-class main:
-
-    class enemy:
+class Main:
+    class Enemy:
         def __init__(self):
             self.id = random.randrange(0, 100)
-            self.internal_enemy_x = 0
-            self.internal_enemy_y = 0
-            self.internal_enemy_width = 0
-            self.internal_enemy_height = 0
-            self.internal_enemy_color = project_colors.light_blue
+            self.enemy_x = 0
+            self.enemy_y = 0
+            self.enemy_width = 0
+            self.enemy_height = 0
+            self.enemy_color = project_colors.light_blue
 
         def enemy(self, enemy_x, enemy_y, enemy_width, enemy_height, enemy_color):
             self.enemy_x = enemy_x
@@ -52,10 +52,10 @@ class main:
 
     @staticmethod
     def message_display(text, height):
-        largeText = pygame.font.Font('freesansbold.ttf', 40)
-        TextSurf, TextRect = text_objects(text, largeText)
-        TextRect.center = ((display_width / 2), height)
-        gameDisplay.blit(TextSurf, TextRect)
+        large_text = pygame.font.Font('freesansbold.ttf', 40)
+        text_surf, text_rect = text_objects(text, large_text)
+        text_rect.center = ((display_width / 2), height)
+        gameDisplay.blit(text_surf, text_rect)
 
         pygame.display.update()
 
@@ -90,7 +90,7 @@ class main:
         enemy_number = 2
         enemy_color = project_colors.light_gray
         for each in range(0, enemy_number):
-            enemy = self.enemy()
+            enemy = self.Enemy()
             # enemy(enemy_x, enemy_y, enemy_width, enemy_height, enemy_color):
             enemy_list.append(enemy.enemy(enemy_start_x, enemy_start_y, enemy_width, enemy_height, enemy_color))
             print(str(enemy.id))
@@ -98,9 +98,9 @@ class main:
         score = 0
 
         self.scoreCounter(score)
-        gameExit = False
+        game_exit = False
 
-        while not gameExit:
+        while not game_exit:
             for event in pygame.event.get():
                 print(event)
                 # If game is intentionally quit
@@ -111,7 +111,7 @@ class main:
                 # if button is pressed
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_LEFT:
-                        x_change = 0-player_speed
+                        x_change = 0 - player_speed
                     elif event.key == pygame.K_RIGHT:
                         x_change = player_speed
                 # if button is released
@@ -132,22 +132,19 @@ class main:
                     enemy.enemy_y = 0 - enemy.enemy_height
                     print("enemy width: " + str(enemy.enemy_width))
                     valid_position = False
-                    # -1 is an invalid value set for identification
                     # TODO: remove no_prev_enemy and use enemy_list position instead
                     if no_prev_enemy:
                         no_prev_enemy = False
                         enemy.enemy_x = random.randrange(0, display_width - int(enemy.enemy_width))
-                        #prev_enemy_x = enemy.enemy_x
-                        #prev_enemy_width = enemy.enemy_width
                     else:
                         while not valid_position:
                             enemy.enemy_x = random.randrange(0, display_width - int(enemy.enemy_width))
 
-
                             margin = 10
-                            if (prev_enemy.enemy_x - margin <= enemy.enemy_x <= prev_enemy.enemy_x + margin + prev_enemy.enemy_width
+                            if (
+                                    prev_enemy.enemy_x - margin <= enemy.enemy_x <= prev_enemy.enemy_x + margin + prev_enemy.enemy_width
                                     or prev_enemy.enemy_x - margin <= enemy.enemy_x + enemy.enemy_width
-                                        <= prev_enemy.enemy_x + margin + prev_enemy.enemy_width):
+                                    <= prev_enemy.enemy_x + margin + prev_enemy.enemy_width):
                                 pass
                             else:
                                 valid_position = True
@@ -166,8 +163,8 @@ class main:
                 if y < enemy.enemy_y + enemy.enemy_height and y + player_height > enemy.enemy_y:
                     # print("y-coordinate crossover")
                     if enemy.enemy_x < x < enemy.enemy_x + enemy.enemy_width \
-                            or enemy.enemy_x  < x + player_width < enemy.enemy_x + enemy.enemy_width:
-                        #print(
+                            or enemy.enemy_x < x + player_width < enemy.enemy_x + enemy.enemy_width:
+                        # print(
                         #    "Enemy hit \nPlayer position: " + str(x) + " to " + str(x + player_width) + " at height " +
                         #    str(y) + "\nEnemy position: " + str(enemy.enemy_x) + " to " +
                         #    str(enemy.enemy_x + enemy.enemy_width)
@@ -184,9 +181,9 @@ class main:
                 self.gameResetAction("You exited the game area. Restarting.")
 
             pygame.display.update()
-            clock.tick(60)
+            clock.tick(90)
 
 
-main().gameLoop()
+Main().gameLoop()
 pygame.quit()
 quit()
